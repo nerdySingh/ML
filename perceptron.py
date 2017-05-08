d11f25a4e8f04803558cad5f69a98c936cf64c04
Tanny <nerdySingh@users.noreply.github.com>
2017-05-08 00:12:37 -0500

    :man_with_turban:

---
+++ perceptron.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy import *
+
+weights = np.array([1,-1,0,0.5])
+net = []
+console = []
+d = np.array([-1,-1,1])
+#x = random.rand(4,4)
+x = np.array([[1,-2,0,-1],[0,1.5,-0.5,-1],[-1,1,0.5,-1]])
+
+for i in np.arange(len(x[0])-1):  
+    temp=0
+    for j in np.arange(len(x[0])):
+        temp = temp + (x[i][j]*weights[j])
+    if d[i] >0 and temp < 0 or d[i]<0 and temp>0 :
+        o = np.sign(temp)
+        for j in np.arange(len(weights)):
+            weights[j] = weights[j]+(0.1 *(d[i] -o)* x[i][j])
+    net.append(temp)
+print(net)
