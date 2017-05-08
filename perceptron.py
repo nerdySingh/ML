7cea3c929559a694e5c15a5c237bd1d9ab6577e4
Tanny <nerdySingh@users.noreply.github.com>
2017-05-08 00:35:30 -0500

    Added the matplot lib implementation to the code :man_with_turban:

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,12 @@
 import numpy as np
 from numpy import *
+import matplotlib.pyplot as plt
 
 weights = np.array([1,-1,0,0.5])
 net = []
 console = []
 d = np.array([-1,-1,1])
-#x = random.rand(4,4)
 x = np.array([[1,-2,0,-1],[0,1.5,-0.5,-1],[-1,1,0.5,-1]])
-
 for i in np.arange(len(x[0])-1):  
     temp=0
     for j in np.arange(len(x[0])):
@@ -17,4 +16,9 @@ for i in np.arange(len(x[0])-1):
         for j in np.arange(len(weights)):
             weights[j] = weights[j]+(0.1 *(d[i] -o)* x[i][j])
     net.append(temp)
+    if net[i]>0:
+        plt.plot(net[i],"r",marker="o")
+    else:
+        plt.plot(net[i],"b",marker="^")
+plt.show()
 print(net)
